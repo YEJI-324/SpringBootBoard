19490292d418f7cd8f6cf033299215f34c6884c7
YEJI-324 <kej980324@gmail.com>
2022-09-16 17:02:32 +0900

    feat : 게시판, 카테고리 entity 추가

--- src/main/java/com/example/testboard/entity/Board.java
+++ src/main/java/com/example/testboard/entity/Board.java
@@ -1,5 +1,17 @@
 package com.example.testboard.entity;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Board {
-    
+
+    private int id;
+    private String title;
+    private String content;
+    private int category;
+    private int cnt;
+    private Date reg_date;
+    private Date mod_date;
 }
--- src/main/java/com/example/testboard/entity/BoardCategory.java
+++ src/main/java/com/example/testboard/entity/BoardCategory.java
@@ -1,4 +1,9 @@
 package com.example.testboard.entity;
 
+import lombok.Data;
+
+@Data
 public class BoardCategory {
+    private int id;
+    private String name;
 }
